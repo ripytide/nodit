2cb87db1eb00e99b5be9b8d134bb518c821f58c3
ripytide <james.forsterer@gmail.com>
2023-04-19 10:09:14 +0100

    point set::get_at_point to map::get_entry_at_point since it has extra semantics

--- src/range_bounds_set.rs
+++ src/range_bounds_set.rs
@@ -66,7 +66,7 @@ where
 	{
 		self.inner.overlapping(range).map(first)
 	}
-	/// See [`RangeBoundsMap::get_at_point()`] for more details.
+	/// See [`RangeBoundsMap::get_entry_at_point()`] for more details.
 	pub fn get_at_point(&self, point: I) -> Result<K, (Bound<I>, Bound<I>)> {
 		self.inner.get_entry_at_point(point).map(first).copied()
 	}
